80a53f0ab688565e3e47d5989e5f11e276d2fe4a
btdf-cmyk <userismail42@gmail.com>
2025-01-31 18:08:27 +0300

    Add files via upload

---
+++ ödev_kolay_3.hafta.py
@@ -0,0 +1,8 @@
+
+vize1 = float(input("Enter your first grade:"))
+vize2 = float(input("Enter your second grade:"))
+ortalama = (vize1 *0.3) + (vize2 *0.7)
+print(f"your  avarage is; {ortalama}")
+fail= (ortalama>=50)
+print(f"geçme durumunuz: {fail}")
+
